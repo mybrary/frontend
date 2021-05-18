3b51ff0e728d65ec944c03dad4cce0ad2bc9d7e6
kortmiranda <kortneymiranda@outlook.com>
2021-05-17 21:58:07 -0500

    add MyBooks component

--- mybrary-client/src/App.js
+++ mybrary-client/src/App.js
@@ -1,41 +1,20 @@
 import React, {useState, useEffect} from "react"
+import MyBooks from './components/MyBooks'
+import { Route } from 'react-router-dom'
 import './App.css';
-import axios from "axios";
+
 import NavBar from './components/Navbar'
 
 
 function App() {
-  const [books, setBooks] = useState([])
-
-  useEffect(() => {
-  function getBooks() {
-    axios.get('/books')
-    .then(res => {
-      setBooks(res.data)
-    })
-    .catch(console.error)
-  }
-  getBooks()
-  }, [])
-
+  
   return (
     <div className="App">
       <NavBar />
-      <div className="book-list">
-      {books.map((book, index) => {
-        return(
-          <ul key={index}>
-          <li>
-          <h2>{book.title}</h2>
-          <h4>{book.author}</h4>
-          <p>Rating : {book.rating}</p>
-          <p>Review: {book.review}</p>
-          </li>
-          </ul>
-        )
-      })}
-    </div>
-    </div>
+    <main>
+      <Route path="/mybooks" exact component={MyBooks} />
+    </main>
+      </div>
   )
 
 }
